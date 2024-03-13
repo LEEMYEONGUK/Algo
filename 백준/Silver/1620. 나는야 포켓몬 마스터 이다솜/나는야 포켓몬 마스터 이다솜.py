0d2f1df88a5d3914fec2290fc9c2724c57f2a191
LEE MYEONGUK <123948643+Djogoat@users.noreply.github.com>
2024-03-13 23:40:38 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 300 ms, Memory: 135016 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+
+p_dic = dict()
+n_dic = dict()
+for i in range(1, N + 1):
+    p = input().strip()
+    p_dic[p] = i
+    n_dic[i] = p
+    
+# print(p_dic)
+# print(n_dic)
+
+for _ in range(M):
+    a = input().strip()
+    if a.isdigit():
+        print(n_dic[int(a)])
+    else:
+        print(p_dic[a])
