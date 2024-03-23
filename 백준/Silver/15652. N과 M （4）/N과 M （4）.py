9016ef1fb4809ee9ec8d872d628b43f70d2dff88
LEE MYEONGUK <123948643+Djogoat@users.noreply.github.com>
2024-03-23 16:45:20 +0900

    [Silver III] Title: N과 M (4), Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,8 +1,5 @@
 def dfs(n, s, lst):
     if n == M:
-        for i in range(M-1):
-            if lst[i] > lst[i + 1]:
-                return
         print(*lst)
         return
     for j in range(s, N + 1):
