1d039c71c5004b1fb49a014054808feb971f9929
LEE MYEONGUK <123948643+Djogoat@users.noreply.github.com>
2024-03-24 09:24:04 +0900

    [Silver II] Title: N과 M (11), Time: 732 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,15 @@
+def dfs(n, tlst):
+    if n == M:
+        print(*tlst)
+        return
+    prev = 0
+    for j in range(N):
+        if prev != lst[j]:
+            prev = lst[j]
+            dfs(n + 1, tlst+[lst[j]])
+
+N, M = map(int, input().split())
+lst = list(map(int, input().split()))
+lst.sort()
+
+dfs(0, [])
