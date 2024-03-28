c62e36fb3b2747f107ef04c8ac78d479310685c2
LEE MYEONGUK <123948643+Djogoat@users.noreply.github.com>
2024-03-28 21:41:43 +0900

    [Silver II] Title: 로또, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,21 @@
+# 로또
+def dfs(n, s, tlst):
+    if n == 6:
+        print(*tlst)
+        return
+    for j in range(s, k + 1):
+        if visited[j] == 0:
+            visited[j] = 1
+            dfs(n + 1, j + 1, tlst+[lst[j]])
+            visited[j] = 0
+
+
+while True:
+    lst = list(map(int, input().split()))
+    if lst[0] == 0:
+        break
+    else:
+        k = lst[0]
+        visited = [0] * (k + 1)
+        dfs(0, 1, [])
+        print()
