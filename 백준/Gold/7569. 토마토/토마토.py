fc1aeb7d76a3627a34607a4543397ff2b7031861
LEE MYEONGUK <123948643+Djogoat@users.noreply.github.com>
2024-04-08 21:31:19 +0900

    [Gold V] Title: 토마토, Time: 760 ms, Memory: 219056 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,33 +1,50 @@
 from collections import deque
 
 def bfs():
-    q = deque()
-    v = [[[0]*M for _ in range(N)] for _ in range(H)]
-
-    cnt = 0
-    for h in range(H):
-        for i in range(N):
-            for j in range(M):
-                if arr[h][i][j] == 1:
-                    q.append((h, i, j))
-                    v[h][i][j] = 1
-                elif arr[h][i][j] == 0:
-                    cnt += 1
+    # 큐에 값이 있는 동안 반복
     while q:
-        ch, ci, cj = q.popleft()
+        h, r, c = q.popleft()
+        # 층, 행, 열 변화
+        for dh, dr, dc in [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, -1), (0, 0, 1)]:
+            nh, nr, nc = h + dh, r + dr, c + dc
+            # 익은 토마토에 인접한 익지 않은 토마토 일때 방문
+            if 0 <= nh < H and 0 <= nr < N and 0 <= nc < M and lst[nh][nr][nc] == 0:
+                lst[nh][nr][nc] = lst[h][r][c] + 1
+                q.append((nh, nr, nc))
 
-        for dh, di, dj in ((0, -1, 0), (0, 1, 0),(0, 0, -1), (0, 0, 1), (1, 0, 0), (-1, 0, 0)):
-            nh, ni, nj = ch+dh, ci+di, cj+dj
-            if 0 <= nh < H and 0 <= ni < N and 0 <= nj < M and v[nh][ni][nj] == 0 and arr[nh][ni][nj] == 0:
-                q.append((nh, ni, nj))
-                v[nh][ni][nj] = v[ch][ci][cj] + 1
-                cnt -= 1
-    if cnt == 0:
-        return v[ch][ci][cj] - 1
+def find():
+    max_v = 1
+    # 배열 순회하며 안 익은 토마토 있는 지 확인
+    for i in range(N):
+        for j in range(M):
+            for k in range(H):
+                max_v = max(max_v, lst[k][i][j])
+                # 0인 값이 익지 않은 토마토이니까 -1 출력하고 반환
+                if lst[k][i][j] == 0:
+                    print(-1)
+                    return
+    # 반복문이 끝났을 때 최댓값이 1이라면 처음부터 다 익은 상태
+    if max_v == 1:
+        print(0)
+        return
+    # 하루가 지나 익은 토마토의 값이 2이니까 최댓값에서 1 빼서 출력
     else:
-        return -1
+        print(max_v - 1)
+        return
 
+
+# 삼차원 배열(층)입력 받기
 M, N, H = map(int, input().split())
-arr = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
-ans = bfs()
-print(ans)
+lst = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
+
+q = deque()
+# 삼차원 배열 순회하며 익은 토마토의 좌표 큐에 넣기
+for i in range(N):
+    for j in range(M):
+        for k in range(H):
+            if lst[k][i][j] == 1:
+                q.append((k, i, j))
+
+# 함수 실행
+bfs()
+find()
