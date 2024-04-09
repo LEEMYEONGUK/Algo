3581588b0ae8cac7bc6ed188f7ed5a1c94281364
LEE MYEONGUK <123948643+Djogoat@users.noreply.github.com>
2024-04-09 09:05:23 +0900

    [Silver II] Title: N과 M (9), Time: 96 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,25 +1,17 @@
-# N과 M (9)
-# 중복 제거
-
 def dfs(n, tlst):
     if n == M:
-        ans.append(tlst)
+        print(*tlst)
         return
     prev = 0
     for j in range(N):
-        if v[j] == 0 and prev != lst[j]:
+        if visited[j] == 0 and prev != lst[j]:
             prev = lst[j]
-            v[j] = 1
-            dfs(n + 1, tlst+[lst[j]])
-            v[j] = 0
-            # 다음 카드 고른 다음에 for j 로 올라가기 때문에 prev 초기 화 x
+            visited[j] = 1
+            dfs(n + 1, tlst + [lst[j]])
+            visited[j] = 0
 
 N, M = map(int, input().split())
 lst = sorted(list(map(int, input().split())))
+visited = [0] * N
 
-ans = []
-v = [0] * N
 dfs(0, [])
-
-for lst in ans:
-    print(*lst)
