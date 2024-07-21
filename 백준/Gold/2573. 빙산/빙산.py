3f5758e46314929c4d3ca36c1b4de891770baafe
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-07-21 14:24:45 +0900

    [Gold IV] Title: 빙산, Time: 772 ms, Memory: 210288 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,69 +1,57 @@
 # 빙산
-import sys
-# sys.setrecursionlimit(10 ** 6)
-input = sys.stdin.readline
 from collections import deque
 
-# 빙산을 녹이는 함수
-def melt(sr, sc):
-    m_visited[sr][sc] = 1
-    m_cnt = 0
-    for d in range(4):
-        r = sr + dr[d]
-        c = sc + dc[d]
-        if 0 <= r < N and 0 <= c < M and arr[r][c] == 0 and m_visited[r][c] == 0:
-            m_cnt += 1
-    if arr[sr][sc] - m_cnt > 0:
-        arr[sr][sc] -= m_cnt
-    else:
-        arr[sr][sc] = 0
-
-# 빙산의 개수 확인하는 dfs
-# def dfs(sr, sc):
-#     visited[sr][sc] = 1
-#     for d in range(4):
-#         r = sr + dr[d]
-#         c = sc + dc[d]
-#         if 0 <= r < N and 0 <= c < M and arr[r][c] != 0 and visited[r][c] == 0:
-#             dfs(r, c)
-
+# 빙산 덩어리 분리된 개수 찾기
 def bfs(sr, sc):
     visited[sr][sc] = 1
     q = deque([(sr, sc)])
     while q:
         r, c = q.popleft()
-        for d in range(4):
-            nr = r + dr[d]
-            nc = c + dc[d]
-            if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] != 0 and visited[nr][nc] == 0:
+        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < N and 0 <= nc < M and lst[nr][nc] != 0 and visited[nr][nc] == 0:
                 q.append((nr, nc))
                 visited[nr][nc] = 1
 
+# 빙산 녹이기
+def melt():
+    melt_lst = [[0] * M for _ in range(N)]
+    for r in range(N):
+        for c in range(M):
+            if lst[r][c] > 0:
+                count = 0
+                for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    nr, nc = r + dr, c + dc
+                    # 주변 바다의 개수 세기
+                    if 0 <= nr < N and 0 <= nc < M and lst[nr][nc] == 0:
+                        count += 1
+                melt_lst[r][c] = count
+    # 주변 바다의 개수 만큼 빼주기
+    for r in range(N):
+        for c in range(M):
+            # 최소값 0 보정
+            lst[r][c] = max(lst[r][c] - melt_lst[r][c], 0)
 
 N, M = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(N)]
 
-arr = [list(map(int, input().split())) for _ in range(N)]
-
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
-
-for i in range(1, 100000):
-    m_visited = [[0] * M for _ in range(N)]
-    for r in range(N):
-        for c in range(M):
-            if arr[r][c]:
-                melt(r, c)
-
-    cnt = 0
+years = 0
+while True:
     visited = [[0] * M for _ in range(N)]
-    for r in range(N):
-        for c in range(M):
-            if arr[r][c] and visited[r][c] == 0:
-                bfs(r, c)
-                cnt += 1
-    if cnt > 1:
-        print(i)
-        break
-    if cnt == 0:
+    land = 0
+    # 덩어리 세기
+    for sr in range(N):
+        for sc in range(M):
+            if lst[sr][sc] != 0 and visited[sr][sc] == 0:
+                bfs(sr, sc)
+                land += 1
+    # 빙산이 다 녹을 때까지 분리 되지 않으면 0
+    if land == 0:
         print(0)
         break
+    if land >= 2:
+        print(years)
+        break
+    # 덩어리가 1개라면 녹이고 1년 증가
+    melt()
+    years += 1
